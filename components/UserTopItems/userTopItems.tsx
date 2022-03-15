74a1396530377262d701294241c978f1ce0d377f
João V. Veras <joaovitorzv@outlook.com>
2022-03-15 12:40:04 -0300

    feat: top items images and sub info

--- components/UserTopItems/userTopItems.tsx
+++ components/UserTopItems/userTopItems.tsx
@@ -2,6 +2,8 @@ import { useTopItems } from "hooks/swr/useTopItems";
 import styles from "./userTopItems.module.scss";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import { FiUsers as FollowersIcon } from "react-icons/fi";
+import { RiAlbumFill as AlbumIcon } from "react-icons/ri";
 
 interface Props {
   type: "topArtists" | "topTracks";
@@ -32,9 +34,38 @@ const UserTopItems = ({ type }: Props) => {
         <h2>Top {favorite[type]}</h2>
       </header>
       <ol className={styles.list}>
-        {data?.items.map((artist: any) => (
-          <li key={artist.name}>{artist.name}</li>
-        ))}
+        {console.log(data)}
+        {type === "topArtists"
+          ? data?.items.map((artist: any) => (
+              <li key={artist.name}>
+                <div className={styles.artistListContainer}>
+                  <img src={artist.images[0].url} alt="a" />
+                  <div className={styles.artistList}>
+                    <p title={artist.name}>{artist.name}</p>
+                    <span>
+                      <FollowersIcon />{" "}
+                      {new Intl.NumberFormat("pt-BR").format(
+                        artist.followers.total
+                      )}{" "}
+                      followers
+                    </span>
+                  </div>
+                </div>
+              </li>
+            ))
+          : data?.items.map((track: any) => (
+              <li key={track.name}>
+                <div className={styles.artistListContainer}>
+                  <img src={track.album.images[0].url} alt="a" />
+                  <div className={styles.artistList}>
+                    <p title={track.name}>{track.name}</p>
+                    <span>
+                      <AlbumIcon /> {track.album.name}
+                    </span>
+                  </div>
+                </div>
+              </li>
+            ))}
       </ol>
     </div>
   );
