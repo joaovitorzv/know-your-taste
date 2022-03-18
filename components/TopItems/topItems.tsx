cc0172f838473c93ffe08d727349d23009550534
João V. Veras <joaovitorzv@outlook.com>
2022-03-18 20:14:58 -0300

    refactor(top-items): swr custom hook conditional types

--- components/TopItems/topItems.tsx
+++ components/TopItems/topItems.tsx
@@ -1,22 +1,9 @@
 import topItems from "./topItems.module.scss";
-import UserTopItems from "components/UserTopItems/userTopItems";
 import { useTopItems } from "hooks/swr/useTopItems";
-import styles from "./userTopItems.module.scss";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import { FiUsers as FollowersIcon } from "react-icons/fi";
 import { RiAlbumFill as AlbumIcon } from "react-icons/ri";
-
-type Artists = {
-  id: string;
-  name: string;
-  images: {
-    url: string;
-  }[];
-  followers: {
-    total: number;
-  };
-}[];
 
 const TopArtists = () => {
   const { data: artists, isLoading } = useTopItems("topArtists");
@@ -53,10 +40,42 @@ const TopArtists = () => {
   );
 };
 
+const TopTracks = () => {
+  const { data: tracks, isLoading } = useTopItems("topTracks");
+
+  if (!tracks || isLoading) {
+    return <Skeleton />;
+  }
+
+  return (
+    <div className={topItems.container}>
+      <header>
+        <h2>Músicas Favoritas</h2>
+      </header>
+      <ol className={topItems.list}>
+        {tracks.items.map((track: any) => (
+          <li key={track.name}>
+            <div className={topItems.artistListContainer}>
+              <img src={track.album.images[0].url} alt={track.name} />
+              <div className={topItems.artistList}>
+                <p title={track.name}>{track.name}</p>
+                <span>
+                  <AlbumIcon /> {track.album.name}
+                </span>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
 const TopItems = () => {
   return (
     <section className={topItems.topItems}>
       <TopArtists />
+      <TopTracks />
     </section>
   );
 };
