50571343a62632ec167d6d6afd2b91475ca478df
João V. Veras <joaovitorzv@outlook.com>
2022-03-18 23:18:29 -0300

    refactor(top-items): list items skeleton pre loading

--- components/TopItems/topItems.tsx
+++ components/TopItems/topItems.tsx
@@ -1,40 +1,66 @@
-import topItems from "./topItems.module.scss";
 import { useTopItems } from "hooks/swr/useTopItems";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { ReactElement } from "react";
 import { FiUsers as FollowersIcon } from "react-icons/fi";
 import { RiAlbumFill as AlbumIcon } from "react-icons/ri";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import topItems from "./topItems.module.scss";
+
+interface ListItemSkeletonWrapperProps {
+  count: number;
+}
+
+const ListItemSkeleton = ({ count }: ListItemSkeletonWrapperProps) => {
+  const listItem = (
+    <li>
+      <div className={topItems.artistListContainer}>
+        <Skeleton height={50} width={50} />
+        <div className={topItems.artistList} style={{ paddingLeft: "10px" }}>
+          <Skeleton height={15} width={150} />
+          <span style={{ marginTop: "10px" }}>
+            <Skeleton height={10} width={70} />
+          </span>
+        </div>
+      </div>
+    </li>
+  );
+  const listItemsSkeleton: JSX.Element[] = [];
+
+  for (let i = 0; i < count; i++) listItemsSkeleton.push(listItem);
+
+  return <>{listItemsSkeleton}</>;
+};
 
 const TopArtists = () => {
   const { data: artists, isLoading } = useTopItems("topArtists");
 
-  if (!artists || isLoading) {
-    return <Skeleton />;
-  }
-
   return (
     <div className={topItems.container}>
       <header>
-        <h2>Artists Favoritos</h2>
+        <h2>Artistas Favoritos</h2>
       </header>
       <ol className={topItems.list}>
-        {artists.items.map((artist) => (
-          <li key={artist.id}>
-            <div className={topItems.artistListContainer}>
-              <img src={artist.images[0].url} alt={artist.name} />
-              <div className={topItems.artistList}>
-                <p title={artist.name}>{artist.name}</p>
-                <span>
-                  <FollowersIcon />
-                  {new Intl.NumberFormat("pt-BR").format(
-                    artist.followers.total
-                  )}{" "}
-                  seguidores
-                </span>
+        {!artists || isLoading ? (
+          <ListItemSkeleton count={5} />
+        ) : (
+          artists.items.map((artist) => (
+            <li key={artist.id}>
+              <div className={topItems.artistListContainer}>
+                <img src={artist.images[0].url} alt={artist.name} />
+                <div className={topItems.artistList}>
+                  <p title={artist.name}>{artist.name}</p>
+                  <span>
+                    <FollowersIcon />
+                    {new Intl.NumberFormat("pt-BR").format(
+                      artist.followers.total
+                    )}{" "}
+                    seguidores
+                  </span>
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
+            </li>
+          ))
+        )}
       </ol>
     </div>
   );
@@ -43,29 +69,29 @@ const TopArtists = () => {
 const TopTracks = () => {
   const { data: tracks, isLoading } = useTopItems("topTracks");
 
-  if (!tracks || isLoading) {
-    return <Skeleton />;
-  }
-
   return (
     <div className={topItems.container}>
       <header>
         <h2>Músicas Favoritas</h2>
       </header>
       <ol className={topItems.list}>
-        {tracks.items.map((track: any) => (
-          <li key={track.name}>
-            <div className={topItems.artistListContainer}>
-              <img src={track.album.images[0].url} alt={track.name} />
-              <div className={topItems.artistList}>
-                <p title={track.name}>{track.name}</p>
-                <span>
-                  <AlbumIcon /> {track.album.name}
-                </span>
+        {!tracks || isLoading ? (
+          <ListItemSkeleton count={5} />
+        ) : (
+          tracks.items.map((track: any) => (
+            <li key={track.name}>
+              <div className={topItems.artistListContainer}>
+                <img src={track.album.images[0].url} alt={track.name} />
+                <div className={topItems.artistList}>
+                  <p title={track.name}>{track.name}</p>
+                  <span>
+                    <AlbumIcon /> {track.album.name}
+                  </span>
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
+            </li>
+          ))
+        )}
       </ol>
     </div>
   );
